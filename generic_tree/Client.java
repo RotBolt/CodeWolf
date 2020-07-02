a0fe2184940e16ef694bcaf2a8a2eea3e87798a1
Rohan Maity <rmrohan03@gmail.com>
2020-07-02 20:51:10 +0530

    Add more problems Generic treee

--- generic_tree/Client.java
+++ generic_tree/Client.java
@@ -3,6 +3,7 @@ package generic_tree;
 public class Client {
 
     // 10 3 20 2 50 0 60 0 30 3 70 0 80 2 110 0 120 0 90 0 40 1 100 0
+    // 10 3 20 1 50 0 30 0 40 1 60 0
     public static void main(String[] args) {
         GenericTree tree = new GenericTree();
         tree.display();
@@ -25,6 +26,35 @@ public class Client {
         tree.levelOrder();
 
         tree.levelOrderLW();
+
+        tree.levelOrderLW2();
+
+        tree.levelOrderZigZag();
+
+        tree.levelOrderZigZagPepVersion();
+
+        // tree.linearize();
+        // tree.display();
+
+        // tree.removeLeaves();
+
+        // tree.display();
+
+        // // changes original data
+        // tree.printMirrorImage();
+
+        // tree.linearizeEffective();
+        // tree.display();
+
+
+        System.out.println(tree.isSymmetric());
+
+        tree.predSucc(120);
+
+        tree.justLarger(83);
+
+        System.out.println(tree.kthSmallest(3));
+
     }
 
 }
